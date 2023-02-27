8a7462b26fcdf76e23262933475e963ca4fce558
prebenrosland <preb1.rosland@gmail.com>
2023-02-27 10:24:50 +0100

    Initial commit

---
+++ Dictionary_module/dictionary_module.mjs
@@ -0,0 +1,30 @@
+
+const english = {
+    btn: "English button",
+    h1: "English headline",
+    joke: "I wrote a song about tortillas.... Actually it's more of a wrap."
+}
+
+const norsk = {
+    btn: "Norsk knapp",
+    h1: "Norsk hovedoverskrift",
+    joke: "To tomater gikk over en vei. En bil kjørte over tomatene."
+}
+
+
+const Dictionary = {
+    en:{
+        "btn": english.btn,
+        "h1": english.h1,
+        "joke": english.joke
+    },
+    no:{
+        "btn": norsk.btn,
+        "h1": norsk.h1,
+        "joke": norsk.joke
+    }
+}
+
+
+
+export default Dictionary;
