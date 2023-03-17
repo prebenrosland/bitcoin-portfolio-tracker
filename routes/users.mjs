3c30d1585bbd9b8c4c2e33dc2226900afef9fe34
prebenrosland <preb1.rosland@gmail.com>
2023-03-17 12:37:57 +0100

    Revert "sql test"
    This reverts commit 2cbb153e4bc05f4f54677531c7ef8619b2660726.

--- routes/users.mjs
+++ routes/users.mjs
@@ -6,7 +6,7 @@ const { Client } = pg.default;
 
 const users = [];
 
-const database = process.env.DATABASE_URL || "postgres://lhpmmsqtyhgmcl:936ad6074f4a587a89be473752a1342a4b799748501a7efad67c569488681d96@ec2-34-251-233-253.eu-west-1.compute.amazonaws.com:5432/d7rhv2l98sse2n";
+const database = process.env.DATABASE_URL;
 
 
 let createDB = new Client(database);
@@ -49,6 +49,8 @@ userRouter.get("/register", (req, res, next) => {
 })
 
 userRouter.post("/register", async (req, res, next) => {
+    const client = new Client(database);
+
     const newUsername = req.body.username;
     for (let i of users){
         if (i.username === newUsername){
@@ -59,21 +61,24 @@ userRouter.post("/register", async (req, res, next) => {
 
     const newPassword = req.body.password += "salt";
     const hash = createHmac('sha256', newPassword).digest('hex');
+
     let balance = 0;
-    let user = {username: newUsername, password: hash, balance: balance};
+    let user = {username: newUsername, password: hash, balance: balance}
     users.push(user);
 
-    const client = new Client(database);
     try {
+
         await client.connect();
         await client.query('INSERT INTO "users" (username, password) VALUES ("preben", "123")');
         client.end();
-        res.send({"status": "ok"});
+
     } catch (error) {
         console.log(error);
-        res.status(500).send({error: 'failed to insert user data into the database'});
-        client.end();
     }
+    
+
+    //res.send(req.body);
+    //res.send(users);
 })
 
 export default userRouter;
