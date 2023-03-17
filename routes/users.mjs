e19fce0b1c2ad7f7be0b2a6c4da1277feda66c01
prebenrosland <preb1.rosland@gmail.com>
2023-03-17 16:24:50 +0100

    Remove late headers

--- routes/users.mjs
+++ routes/users.mjs
@@ -16,17 +16,6 @@ const credentials = {
 }
 
 
-/*let createDB = new Client(database);
-createDB.connect();
-await createDB.query('CREATE DATABASE mydatabase');
-createDB.close();*/
-
-
-
-userRouter.get("/login", (req, res, next) => {
-    res.send({data: "your data"})
-})
-
 userRouter.post("/login", async (req, res, next) => {
     const newPassword = req.body.password += "salt";
     const hash = createHmac('sha256', newPassword).digest('hex');
@@ -42,13 +31,12 @@ userRouter.post("/login", async (req, res, next) => {
         let result = await client.query('SELECT * FROM users where username = $1 AND password = $2', [req.body.username, hash]);
         if (result.rowCount > 0){
             let balance = result.rows[0].balance;
-            res.send({"login":"ok", "balance": balance});
+            res.status(200).send({"login":"ok", "balance": balance});
         }
         else{
             res.send({error: "Wrong username or password"});
         }
         await client.end();
-        res.send({"status": "ok"});
     } catch (error) {
         console.log(error);
         res.status(500).send({error: 'failed to login'});
@@ -69,25 +57,17 @@ userRouter.put("/login", async (req, res, next) => {
         await client.connect();
         let result = await client.query('UPDATE users SET balance=$1 WHERE username=$2 RETURNING balance', [req.body.balance, req.body.username]);
 
-        if (result.rowCount > 0) {
-            let balance = result.rows[0].balance;
-            res.send({"transaction": "ok", "balance": balance});
-          }
+        let balance = result.rows[0].balance;
+        res.send({"transaction": "ok", "balance": balance});
 
         await client.end();
-        res.send({"status": "ok"});
     } catch (error) {
         console.log(error);
         res.status(500).send({error: 'failed to login'});
         client.end();
     }
-    res.status(400).send({"error": "invalid username or password"});
 })
 
-
-userRouter.get("/register", (req, res, next) => {
-    res.send({data: "your data"})
-})
 
 userRouter.post("/register", async (req, res, next) => {
     const newUsername = req.body.username;
