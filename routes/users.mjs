574a178054b365f9a2fa858df90077415986f892
prebenrosland <preb1.rosland@gmail.com>
2023-03-17 13:04:15 +0100

    a

--- routes/users.mjs
+++ routes/users.mjs
@@ -9,10 +9,10 @@ const users = [];
 const database = process.env.DATABASE_URL;
 
 
-let createDB = new Client(database);
+/*let createDB = new Client(database);
 createDB.connect();
 await createDB.query('CREATE DATABASE mydatabase');
-createDB.close();
+createDB.close();*/
 
 
 
@@ -49,8 +49,6 @@ userRouter.get("/register", (req, res, next) => {
 })
 
 userRouter.post("/register", async (req, res, next) => {
-    const client = new Client(database);
-
     const newUsername = req.body.username;
     for (let i of users){
         if (i.username === newUsername){
@@ -61,24 +59,21 @@ userRouter.post("/register", async (req, res, next) => {
 
     const newPassword = req.body.password += "salt";
     const hash = createHmac('sha256', newPassword).digest('hex');
-
     let balance = 0;
-    let user = {username: newUsername, password: hash, balance: balance}
+    let user = {username: newUsername, password: hash, balance: balance};
     users.push(user);
 
+    const client = new Client(database);
     try {
-
         await client.connect();
-        await client.query('INSERT INTO "users" (username, password) VALUES ("preben", "123")');
+        await client.query('INSERT INTO users (username, password) VALUES (preben, 123)');
         client.end();
-
+        res.send({"status": "ok"});
     } catch (error) {
         console.log(error);
+        res.status(500).send({error: 'failed to insert user data into the database'});
+        client.end();
     }
-    
-
-    //res.send(req.body);
-    //res.send(users);
 })
 
 export default userRouter;
